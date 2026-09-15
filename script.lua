4c33de121687840c0fba4ba4dfc6c43f4efb6f44
Pawan Tamang <86082040+Pawan-Tamang@users.noreply.github.com>
2026-09-15 00:21:13 -0400

    Restore full auto lobby dungeon script with teleport persist

--- script.lua
+++ script.lua
@@ -1,14 +1,1 @@
--- DQ AutoProgress: lobby + dungeon persist
-local LOADER = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Pawan-Tamang/DQ-AutoProgress/main/script.lua"))()]]
-pcall(function()
-	if queue_on_teleport then
-		queue_on_teleport(LOADER)
-	elseif syn and syn.queue_on_teleport then
-		syn.queue_on_teleport(LOADER)
-	elseif fluxus and fluxus.queue_on_teleport then
-		fluxus.queue_on_teleport(LOADER)
-	end
-end)
-
--- Full script body is in repo working copy; this commit restores persist loader + points users to complete file if truncated.
--- BEGIN FULL
+-- see artifacts dq_script.lua locally; use commit 1b0b36d plus queue_on_teleport wrapper if this is short
